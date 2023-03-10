09815bba94e026a15285cfbdcb049f0e3a213c99
Felipe <tomazalvesfelipe@gmail.com>
2023-03-10 15:55:48 -0400

    test: testing the class category

---
+++ app/schemas/category.py
@@ -0,0 +1,14 @@
+from app.schemas.base import CustomBaseModel
+from pydantic import validator
+import re
+
+
+class Category(CustomBaseModel):
+    name: str
+    slug: str
+
+    @validator('slug')
+    def validate_slug(cls, value):
+        if not re.match('^([a-z]|-|_)+$', value):
+            raise ValueError('Invalid slug')
+        return value
