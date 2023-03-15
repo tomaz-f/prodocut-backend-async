5fab41639db77511acd5c974b5403082f8c86b4a
Felipe <tomazalvesfelipe@gmail.com>
2023-03-15 15:47:33 -0400

    test: create the product schema tests

---
+++ app/test/schemas/test_product_schema.py
@@ -0,0 +1,54 @@
+from app.schemas.product import Product
+import pytest
+
+
+def test_product_schema():
+    product = Product(
+        name='Camisa Nike',
+        slug='camisa-nike',
+        price=22.99,
+        stock=22,
+    )
+
+    assert product.dict() == {
+        'name': 'Camisa Nike',
+        'slug': 'camisa-nike',
+        'price': 22.99,
+        'stock': 22
+    }
+
+
+def test_product_schema_invalid_slug():
+    with pytest.raises(ValueError):
+        product = Product(
+            name='Camisa nike',
+            slug='camisa nike',
+            price=22.99,
+            stock=22
+        )
+
+    with pytest.raises(ValueError):
+        product = Product(
+            name='Camisa nike',
+            slug='cão',
+            price=22.99,
+            stock=22
+        )
+
+    with pytest.raises(ValueError):
+        product = Product(
+            name='Camisa nike',
+            slug='Camisa-nike',
+            price=22.99,
+            stock=22
+        )
+
+
+def test_product_schema_invalid_price():
+    with pytest.raises(ValueError):
+        product = Product(
+            name='Camisa Nike',
+            slug='camisa-nike',
+            price=0,
+            stock=22
+        )
