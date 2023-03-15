f5563aef8c0a6856631095aaf2653cbce8710778
Felipe <tomazalvesfelipe@gmail.com>
2023-03-15 09:24:45 -0400

    fix: function serialize indentention

--- app/use_cases/category.py
+++ app/use_cases/category.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from app.db.models import Category as CategoryModel
 from app.schemas.category import Category, CategoryOutput
 from sqlalchemy.orm import Session
@@ -19,6 +20,16 @@ class CategoryUseCases:
             for category_model in categories_on_db
         ]
         return categories_output
+
+    def delete_category(self, id: int):
+        category_model = self.db_session.query(
+            CategoryModel).filter_by(id=id).first()
+        if not category_model:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail='Category not found')
+
+        self.db_session.delete(category_model)
+        self.db_session.commit()
 
     def serialize_category(self, category_model: CategoryModel):
         return CategoryOutput(**category_model.__dict__)
