8ce37be4e0de21d6264c688a529989881cb182f0
Felipe <tomazalvesfelipe@gmail.com>
2023-03-16 14:40:42 -0400

    feat: create the delete product use case

--- app/use_cases/product.py
+++ app/use_cases/product.py
@@ -40,3 +40,14 @@ class ProductUseCases:
 
         self.db_session.add(product_on_db)
         self.db_session.commit()
+
+    def delete_product(self, id: int):
+        product_on_db = self.db_session.query(
+            ProductModel).filter_by(id=id).first()
+
+        if product_on_db is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail='No prouct was found')
+
+        self.db_session.delete(product_on_db)
+        self.db_session.commit()
