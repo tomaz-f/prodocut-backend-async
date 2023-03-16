00cbcefc74b5da7b8bbb706627e7a4ce74761765
Felipe <tomazalvesfelipe@gmail.com>
2023-03-16 13:50:13 -0400

    test: create the update product test

--- app/test/conftest.py
+++ app/test/conftest.py
@@ -1,6 +1,7 @@
 from app.db.connection import Session
 import pytest
 from app.db.models import Category as CategoryModels
+from app.db.models import Product as ProductModel
 
 
 @pytest.fixture()
@@ -32,4 +33,22 @@ def categories_on_db(db_session):
 
     for category in categories:
         db_session.delete(category)
+    db_session.commit()
+
+
+@pytest.fixture()
+def product_on_db(db_session):
+    product = ProductModel(
+        name='Camisa-adidas',
+        slug='camisa-adidas',
+        price=100.99,
+        stock=20
+    )
+
+    db_session.add(product)
+    db_session.commit()
+
+    yield product
+
+    db_session.delete(product)
     db_session.commit()
--- app/test/use_cases/test_product_use_cases.py
+++ app/test/use_cases/test_product_use_cases.py
@@ -42,3 +42,38 @@ def test_add_product_uc_invalid_category(db_session):
 
     with pytest.raises(HTTPException):
         uc.add_product(product=product, category_slug='invalid')
+
+
+def test_update_product(db_session, product_on_db):
+    product = Product(
+        name='Camisa Nike',
+        slug='camisa-nike',
+        price=22.99,
+        stock=22
+    )
+
+    uc = ProductUseCases(db_session=db_session)
+    uc.update_product(id=product_on_db.id, product=product)
+
+    product_updated_on_db = db_session.query(
+        ProductModel).filter_by(id=product_on_db.id)
+
+    assert product_updated_on_db is not None
+    assert product_updated_on_db.name == product.name
+    assert product_updated_on_db.slug == product.slug
+    assert product_updated_on_db.price == product.price
+    assert product_updated_on_db.stock == product.stock
+
+
+def test_update_product_invalid_id(db_session, product_on_db):
+    product = Product(
+        name='Camisa Nike',
+        slug='camisa-nike',
+        price=22.99,
+        stock=22
+    )
+
+    uc = ProductUseCases(db_session=db_session)
+
+    with pytest.raises(HTTPException):
+        uc.update_product(id=1, product=product)
