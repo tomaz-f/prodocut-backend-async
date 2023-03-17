9cef1f8159a00582f446c6292689dfc9b242893c
Felipe Tomaz <tomazalvesfelipe@gmail.com>
2023-03-17 15:11:08 -0400

    feat: user schema validation

---
+++ app/schemas/user.py
@@ -0,0 +1,14 @@
+from app.schemas.base import CustomBaseModel
+from pydantic import validator
+import re
+
+
+class User(CustomBaseModel):
+    username: str
+    password: str
+
+    @validator('username')
+    def validate_username(cls, value):
+        if not re.match('^([a-z]|[A-Z]|[0-9]|-|_|@)+$', value):
+            raise ValueError('Invalid username')
+        return value
