8e83d89827e9ea1b1f65aba410dba0ccf281a1e0
Felipe Tomaz <tomazalvesfelipe@gmail.com>
2023-03-17 15:59:11 -0400

    feat: create user route and add to main app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.routes.category_routes import router as category_routes
 from app.routes.product_routes import router as product_routes
-
+from app.routes.user_routes import router as user_routes
 
 app = FastAPI()
 
@@ -13,3 +13,4 @@ def health_check():
 
 app.include_router(category_routes)
 app.include_router(product_routes)
+app.include_router(user_routes)
