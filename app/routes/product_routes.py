fd66fecadba4497f058361060699a6d586b417a3
Felipe Tomaz <tomazalvesfelipe@gmail.com>
2023-03-17 14:04:40 -0400

    feat: add search parameter to url

--- app/routes/product_routes.py
+++ app/routes/product_routes.py
@@ -46,8 +46,8 @@ def update_product(
 
 
 @router.get('/list')
-def list_product(db_session: Session = Depends(get_db_session)):
+def list_product(search: str = '', db_session: Session = Depends(get_db_session)):
     uc = ProductUseCases(db_session=db_session)
-    products = uc.list_products()
+    products = uc.list_products(search=search)
 
     return products
