4b9f84d018498a82e404dd9fc581418dde113430
Felipe Tomaz <tomazalvesfelipe@gmail.com>
2023-03-23 13:43:49 -0400

    test: user token data test validation

--- app/test/schemas/test_user_schema.py
+++ app/test/schemas/test_user_schema.py
@@ -1,5 +1,6 @@
-from app.schemas.user import User
+from app.schemas.user import User, TokenData
 import pytest
+from datetime import datetime
 
 
 def test_user_schema():
@@ -13,3 +14,16 @@ def test_user_schema():
 def test_user_schema_invalid_username():
     with pytest.raises(ValueError):
         user = User(username='João#', password='nsi#1234')
+
+
+def test_token_date():
+    expires_at = datetime.now()
+    token_data = TokenData(
+        access_token='token qualquer',
+        expires_at=expires_at
+    )
+
+    assert token_data.dict() == {
+        'access_token': 'token qualquer',
+        'expires_at': expires_at
+    }
