ba61f2d2e609faeefbae9e54f15faba9294c615a
Felipe Tomaz <tomazalvesfelipe@gmail.com>
2023-03-23 14:31:22 -0400

    feat: user login route

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from app.routes.deps import get_db_session
 from app.schemas.user import User
 from app.use_cases.user import UserUseCases
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(prefix='/user', tags=['User'])
 
@@ -15,3 +16,18 @@ def user_register(
     uc = UserUseCases(db_session=db_session)
     uc.register_user(user=user)
     return Response(status_code=status.HTTP_201_CREATED)
+
+
+@router.post('/login')
+def user_login(login_request_form: OAuth2PasswordRequestForm = Depends(),
+               db_session: Session = Depends(get_db_session)):
+    uc = UserUseCases(db_session=db_session)
+
+    user = User(
+        username=login_request_form.username,
+        password=login_request_form.password
+    )
+
+    token_data = uc.user_login(user=user, expires_in=60)
+
+    return token_data
