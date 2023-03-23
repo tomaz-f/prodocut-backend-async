1aa694effd44eaa418f44da7ac0922edf91105b0
Felipe Tomaz <tomazalvesfelipe@gmail.com>
2023-03-23 13:44:09 -0400

    feat: user validation token schema

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app.schemas.base import CustomBaseModel
 from pydantic import validator
 import re
@@ -12,3 +13,8 @@ class User(CustomBaseModel):
         if not re.match('^([a-z]|[A-Z]|[0-9]|-|_|@)+$', value):
             raise ValueError('Invalid username')
         return value
+
+
+class TokenData(CustomBaseModel):
+    access_token: str
+    expires_at: datetime
